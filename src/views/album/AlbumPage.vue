7756bc0b1190b503607ef7bcff7e7a1de9841771
YannisCheng <cwj1714@163.com>
2020-06-18 15:15:24 +0800

    梳理'slot'基本使用、作用域传值问题

--- src/views/album/AlbumPage.vue
+++ src/views/album/AlbumPage.vue
@@ -1,12 +1,69 @@
 <template>
-  <div >
-  hello
+  <div style="width: 100%">
+
+    <!--展示 'ListIemSlot' 组件内部各种slot-->
+    <ListIemSlot v-bind:array-data="arrayData">
+
+      <!--下面这种写法是'<template v-slot:default>'的简写-->
+      <template>
+        01: 展示名为"default"的slot内容
+      </template>
+
+      <template v-slot:slot_01>
+        <span style="background: #42b983;color: white">
+          02: 展示名为"slot_01"的slot内容
+        </span>
+      </template>
+
+      <template v-slot:slot_data="dataFromChild">
+        <h5>
+          03：展示名为"slot_data"的slot内容,数据来自子组件
+        </h5>
+        <div style="background: darkorange;color: white;height: 40px">
+          {{dataFromChild.data}}
+        </div>
+      </template>
+
+      <!--
+      绑定在slot上的attribute称为'slot prop',
+      在父级作用域中用带'值'的v-slot来定义我们提供的'slot prop'名字。
+      在这里将包含所有插槽 prop 的对象命名为'itemP'
+      -->
+      <template v-slot:itemSlot="itemP">
+        <div style="background: lightgrey;height: 30px;margin-top: 10px;">
+          {{itemP.itemProp.iType}}:{{itemP.itemProp.iName}}:{{itemP.itemProp.iAddress}}
+        </div>
+      </template>
+    </ListIemSlot>
   </div>
 </template>
 
 <script>
+import ListIemSlot from '@/views/album/ListIemSlot'
+
 export default {
-  name: 'album_page'
+  name: 'album_page',
+  components: { ListIemSlot },
+  data () {
+    return {
+      user: {
+        firstName: '11111',
+        lastName: 'aaaaa'
+      },
+      arrayData: [
+        { iName: 'name_01', iType: '1', iAddress: 'Add_01' },
+        { iName: 'name_02', iType: '2', iAddress: 'Add_02' },
+        { iName: 'name_03', iType: '3', iAddress: 'Add_03' },
+        { iName: 'name_04', iType: '4', iAddress: 'Add_04' },
+        { iName: 'name_05', iType: '5', iAddress: 'Add_05' },
+        { iName: 'name_06', iType: '6', iAddress: 'Add_06' },
+        { iName: 'name_07', iType: '7', iAddress: 'Add_07' },
+        { iName: 'name_08', iType: '8', iAddress: 'Add_08' },
+        { iName: 'name_09', iType: '9', iAddress: 'Add_09' },
+        { iName: 'name_10', iType: '10', iAddress: 'Add_10' },
+      ],
+    }
+  },
 }
 </script>
 
---
+++ src/views/album/ListIemSlot.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+
+    <h5>
+      <slot></slot>
+    </h5>
+
+    <h5>
+      <slot name="slot_01"></slot>
+    </h5>
+
+    <slot name="slot_data" v-bind:data="data"></slot>
+
+    <div>
+      <h6>04：列表组件插槽样式展示：<br/>
+        在"父级作用域"下的"slot模板内渲染列表的ItemView"，<br/>其中渲染的数据来自"由父级传给子级"</h6>
+      <ol style="padding-inline-start: 0">
+        <li style="list-style: none;" v-for="(item) in arrayData" :key="item.iType">
+          <!--将'item'作为'slot'的元素的一个attribute，绑定到slot。
+          供在父级作用域中'具体声明此slot时使用此数据'-->
+          <slot name="itemSlot" v-bind:itemProp="item">
+            <!--可有可无-->
+            <!--{{item.iName}}:{{item.iType}}:{{item.iAddress}}-->
+          </slot>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListIemSlot',
+  data () {
+    return {
+      data: ['zhangsan', 'lisi', 'wanwu', 'zhaoliu', 'tianqi', 'xiaoba']
+    }
+  },
+  props: ['arrayData']
+}
+</script>
+
+<style scoped>
+
+</style>
